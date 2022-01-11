b10ef1ccc29383b347e47bd510b3d150de13e6a1
DESKTOP-2UGHQVH\anduser <a.kwoka@andersenlab.com>
2022-01-11 19:30:19 +0100

    implement search fuctionality and change favicon and title

--- picsme_fe/src/components/Search/Search.jsx
+++ picsme_fe/src/components/Search/Search.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import MasonryLayout from '../MasonryLayout/MasonryLayout';
+import { client } from '../../client';
+import { feedQuery, searchQuery } from '../../utils/data';
+import Spinner from '../Spinner/Spinner';
 
 import './Search.css';
 
-const Search = () => {
+const Search = ( {searchTerm }) => {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if(searchTerm) {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+    } else {
+      client.fetch(feedQuery)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+    }
+  }, [searchTerm]);
   return (
     <div>
-      search
+      {loading && <Spinner message="Searching fo pins..." />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins}/>}
+      {pins?.length === 0 && searchTerm !== '' && !loading && (
+        <div className='search-not-found'>
+          No pins found!
+        </div>
+      )}
     </div>
   )
 }
